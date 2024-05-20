9706e27261cecf0d689d2f83384b26a2de704a12
wareez92 <wareezsowemimo@gmail.com>
2024-05-20 13:48:56 -0500

    Rendered div w/ name, age and email for each pup

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,32 @@ function App() {
   const [featPupId, setFeatPupId] = useState(null);
 
   console.log("puppyList: ", puppyList);
+  
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <>
       <div className="App">
+        {featPupId && <div>
+          <h2>{featuredPup.name} </h2>
+          <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+</ul>
+</div>}
+
         {puppies.map((puppy) => {
-          return <p onClick={() =>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>{puppy.name} </p>;
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}{" "}
+            </p>
+          );
         })}
       </div>
     </>
